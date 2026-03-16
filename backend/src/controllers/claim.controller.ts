507e261d14c2083dd59981b412f9db5268097e5a
raphaellndr <raph.landure@gmail.com>
2026-03-16 12:55:25 +0100

    Implement claims controllers

---
+++ backend/src/controllers/claim.controller.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+
+import { Prisma } from "../generated/prisma/client";
+import { ClaimService } from "../services/claim.service";
+
+export const getClaims = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const claims = await ClaimService.findAll();
+
+    return res.status(200).json({ data: claims });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getClaim = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = Number(req.params.id);
+    const claim = await ClaimService.findById(id);
+
+    if (!claim) {
+      return res.status(404).json({ error: `No claim found with id '${id}'` });
+    }
+    return res.status(200).json({ data: claim });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createClaim = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = req.body as Prisma.ClaimCreateInput;
+    const claim = await ClaimService.create(data);
+
+    return res.status(201).json({ data: claim });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateClaim = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = Number(req.params.id);
+    const data = req.body as Prisma.ClaimUpdateInput;
+    const claim = await ClaimService.update(id, data);
+
+    return res.status(200).json({ data: claim });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteClaim = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = Number(req.params.id);
+    await ClaimService.delete(id);
+
+    return res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
